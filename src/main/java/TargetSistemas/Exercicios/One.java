d677411b63ba6ce3fa6684a63034569521bd69bf
nalvescabrera <n.alvescabrera@gmail.com>
2025-01-15 23:07:35 -0300

    Implementação exercicio One

---
+++ src/main/java/TargetSistemas/Exercicios/One.java
@@ -0,0 +1,16 @@
+package TargetSistemas.Exercicios;
+
+public class One {
+
+    public int exerciseOne(){
+        int indice = 13, soma = 0, k = 0;
+
+        while (k <indice){
+            k = k+1;
+            soma = soma + k;
+        }
+        System.out.println(soma);
+        return indice;
+    }
+
+}
